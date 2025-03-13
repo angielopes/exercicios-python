abec560a4d31f309943e8f1ce38a626d9a660a8f
Angie <i4ngela@gmail.com>
2025-03-13 13:58:52 -0300

    Enhance ShoppingList class: Add detailed docstrings for methods and class attributes

--- exercicios/1_faceis/listas/001_shopping_list_solved.py
+++ exercicios/1_faceis/listas/001_shopping_list_solved.py
@@ -10,25 +10,71 @@ The program must:
 
 
 class ShoppingList:
+    """
+    A class to represent a shopping list.
+
+    Attributes
+    ----------
+    shopping_list : list
+        a list to store shopping items
+
+    Methods
+    -------
+    add_item(item):
+        Adds an item to the shopping list if it is not already present.
+    sort_list():
+        Sorts the shopping list in alphabetical order and displays it.
+    remove_item(item):
+        Removes an item from the shopping list if it is present.
+    display_list():
+        Displays the current shopping list.
+    """
 
     def __init__(self):
-        # docstring
+        """
+        Initializes a new instance of the shopping list.
+        This constructor initializes an empty shopping list.
+        """
+        
         self.shopping_list = []
 
     def add_item(self, item):
-        # docstring
+        """
+        Adds an item to the shopping list.
+        Args:
+            item (str): The item to be added to the shopping list.
+        Returns:
+            None
+        Prints a message if the item is already in the shopping list.
+        """
+        
         if item in self.shopping_list:
             print("Item already added!")
         else:
             self.shopping_list.append(item)
 
     def sort_list(self):
-        # docstring
+        """
+        Sorts the shopping list in alphabetical order and displays the sorted list.
+        This method sorts the items in the `shopping_list` attribute in place using
+        the default sorting order (alphabetical). After sorting, it calls the 
+        `display_list` method to show the sorted list to the user.
+        """
+        
         self.shopping_list.sort()
         self.display_list()
 
     def remove_item(self, item):
-        # docstring
+        """
+        Removes an item from the shopping list.
+        Parameters:
+            item (str): The item to be removed from the shopping list.
+        Returns:
+            None
+        If the item is found in the shopping list, it is removed and a message 
+        "Item removed!" is printed, followed by the updated shopping list. 
+        If the item is not found, a message "This item is not in the list." is printed.
+        """
         if item in self.shopping_list:
             self.shopping_list.remove(item)
             print("Item removed!")
@@ -37,6 +83,12 @@ class ShoppingList:
             print("This item is not in the list.")
 
     def display_list(self):
+        """
+        Displays the shopping list.
+        This method prints each item in the shopping list with the first letter
+        of each word capitalized.
+        """
+
         print("Your shopping list:")
         for item in self.shopping_list:
             print(item.title())
