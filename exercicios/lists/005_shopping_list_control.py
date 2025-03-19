902a967c22a6fe91763a23446421040ccea26da9
Angie <i4ngela@gmail.com>
2025-03-19 10:15:26 -0300

    Code translate from Brazilian Portuguese to English

--- exercicios/lists/005_shopping_list_control.py
+++ exercicios/lists/005_shopping_list_control.py
@@ -1,92 +1,94 @@
-"""Crie um programa para gerenciar uma lista de compras.
-Cada item terá um nome do produto, sua quantidade e o preço unitário. O programa deverá permitir:
-1. Adicionar um produto à lista de compras.
-2. Remover um produto da lista.
-3. Calcular o valor total da compra.
-4. Exibir todos os itens com nome, quantidade e preço total de cada produto."""
+"""Create a program to manage a shopping list.
+Each item will have a product name, its quantity, and the unit price. The program should allow:
+1. Adding a product to the shopping list.
+2. Removing a product from the list.
+3. Calculating the total purchase value.
+4. Displaying all items with the name, quantity, and total price of each product."""
 
-lista_compra = []
+shopping_list = []
 
 while True:
     print(
         """
-            Opções:
-    [1] Adicionar produto
-    [2] Remover produto
-    [3] Ver lista de compras
-    [4] Calcular total
-    [5] Sair"""
+            Options:
+    [1] Add product
+    [2] Remove product
+    [3] View shopping list
+    [4] Calculate total
+    [5] Exit"""
     )
 
     try:
-        opcao = int(input("\nEscolha a opção: "))
+        option = int(input("\nChoose an option: "))
     except ValueError:
-        print("Por favor, digite um número válido.")
+        print("Please enter a valid number.")
         continue
 
-    # Sair do programa
-    if opcao == 5:
-        print("Programa encerrado...")
+    # Exit the program
+    if option == 5:
+        print("Program closed...")
         break
 
-    # Adicionar produtos, quantidade e valor
-    elif opcao == 1:
-        produto = input("Nome do produto: ").capitalize().strip()
+    # Add products, quantity, and price
+    elif option == 1:
+        product = input("Product name: ").capitalize().strip()
         try:
-            qtd_produto = int(input("Quantidade: "))
-            preco_unitario = float(input("Preço unitário: "))
+            product_qty = int(input("Quantity: "))
+            unit_price = float(input("Unit price: "))
         except ValueError:
-            print("Quantidade e preço preciam ser números válidos.")
+            print("Quantity and price must be valid numbers.")
             continue
 
-        # Verificar se o item já existe (se existir, irá apenas adicionar quantidade)
-        for item in lista_compra:
-            if item[0] == produto:
-                item[1] += qtd_produto
+        # Check if the item already exists (if it exists, just add the quantity)
+        for item in shopping_list:
+            if item[0] == product:
+                item[1] += product_qty
                 print(
-                    f"{qtd_produto} unidades adicionadas ao produto {produto} na lista de compra."
+                    f"{product_qty} units added to the product {product} in the shopping list."
                 )
                 break
         else:
-            lista_compra.append([produto, qtd_produto, preco_unitario])
+            shopping_list.append([product, product_qty, unit_price])
 
-    # Remover produtos
-    elif opcao == 2:
-        remover_produto = (
-            input("Qual produto deseja remover da lista? ").capitalize().strip()
+    # Remove products
+    elif option == 2:
+        remove_product = (
+            input("Which product do you want to remove from the list? ")
+            .capitalize()
+            .strip()
         )
-        for item in lista_compra:
-            if item[0] == remover_produto:
-                lista_compra.remove(item)
-                print(f"{remover_produto} removido da lista.")
+        for item in shopping_list:
+            if item[0] == remove_product:
+                shopping_list.remove(item)
+                print(f"{remove_product} removed from the list.")
                 break
         else:
-            print(f"O produto {remover_produto} não está na lista de compra.")
+            print(f"The product {remove_product} is not in the shopping list.")
 
-    # Visualizar lista de compra
-    elif opcao == 3:
-        if lista_compra:
-            print("\nLISTA DE COMPRA:")
-            for item in lista_compra:
-                valor_total_produto = item[1] * item[2]
+    # View shopping list
+    elif option == 3:
+        if shopping_list:
+            print("\nSHOPPING LIST:")
+            for item in shopping_list:
+                total_product_value = item[1] * item[2]
                 print(
-                    f"{item[0]} - Quantidade: {item[1]} - Preço Unitário: R$ {item[2]:.2f} - Total: R$ {valor_total_produto:.2f}"
+                    f"{item[0]} - Quantity: {item[1]} - Unit Price: $ {item[2]:.2f} - Total: $ {total_product_value:.2f}"
                 )
         else:
-            print("Lista de compras está vázia.")
+            print("Shopping list is empty.")
 
-    # Calcular valor total da compra
-    elif opcao == 4:
-        if lista_compra:
-            valor_total_compra = sum(item[1] * item[2] for item in lista_compra)
-            print(f"\nValor total da compra: R$ {valor_total_compra}")
+    # Calculate total purchase value
+    elif option == 4:
+        if shopping_list:
+            total_purchase_value = sum(item[1] * item[2] for item in shopping_list)
+            print(f"\nTotal purchase value: $ {total_purchase_value}")
         else:
-            print("Lista de compras está vázia.")
+            print("Shopping list is empty.")
 
     else:
-        print("Opção inválida. Tente novamente.")
+        print("Invalid option. Try again.")
 
-"""É mesma coisa que
-    valor_total_compra = 0
-    for item in lista_compras:
-        valor_total_compras += item[1] * item[2]"""
+"""It's the same as
+    total_purchase_value = 0
+    for item in shopping_list:
+        total_purchase_value += item[1] * item[2]"""
